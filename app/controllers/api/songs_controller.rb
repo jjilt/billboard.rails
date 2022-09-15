ab699fcab27541d30db2a1e17131cabb9901aee4
brennenlovato <111826208+brennenlovato@users.noreply.github.com>
2022-09-15 17:27:33 -0600

    Update songs_controller.rb

--- app/controllers/api/songs_controller.rb
+++ app/controllers/api/songs_controller.rb
@@ -1,16 +1,49 @@
 class Api::SongsController < ApplicationController
+  # model name is comment 
+  # follow the pattern to fill the controller out 
+  # then change to the parent and child relationship  
+
+  # optional, callback, before_action to clean up the show update and destroy
   def index
-  end
+    @song = Song.all
+    render component: 'Song', props:{ song: @song }
+   end
 
-  def show
-  end
+   def show
+    @song = Song.find(params[:id])
+    render component: 'Song', props: { song: @song }
+   end
 
-  def create
+   def create
+    @song = Song.new(song_params)
+    if @song.save
+      do something or do somewhere
+    else
+      render component: 'SongNew', props: { song: @song }
+    end
   end
 
   def update
+    @song = Song.find(params[:id])
+    if @song.update(song_params)
+      do something
+    else
+      render component: 'SongEdit', props: { song: @song }
+    end
   end
 
   def destroy
+    @song = Song.find(params[:id])
+    @song.destroy
+    send somewhere
+    or
+    Song.find(params[:id]).destroy
+    send somewhere
   end
+  
+  private
+  def song_params
+    params.require(:song).permit(:attr, :attr2, :everything the table has)
+  end
+
 end
